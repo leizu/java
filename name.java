3973eafcb73b9553d2308b6b9244d321eea27377
leizu <21311154ys@tama.ac.jp>
2014-10-08 15:46:16 +0900

    3回目のプログラム

---
+++ name.java
@@ -0,0 +1,10 @@
+class name
+{
+ public static void main(String argv[])
+	{
+	String name;
+	System.out.print("‚ ‚È‚½‚Ì–¼‘O‚ÍH");
+	name = new java.util.Scanner(System.in).nextLine();
+	System.out.println( name +"‚³‚ñA‚±‚ñ‚É‚¿‚Í");
+	}
+}
