1e4df9bd88525d3abfad59345a91e7ac0455df25
leizu <21311154ys@tama.ac.jp>
2014-10-15 15:27:52 +0900

    21311154

---
+++ keisan.java
@@ -0,0 +1,27 @@
+class keisan
+{
+ public static void main(String argv[])
+ {
+ double suuzi1;
+ double suuzi2;
+ double tasi;
+ double hiki;
+ double kake;
+ double wari;
+ double amari;
+ System.out.print("”š‚P=");
+ suuzi1 = new java.util.Scanner(System.in).nextDouble();
+ System.out.print("”š‚Q=");
+ suuzi2 = new java.util.Scanner(System.in).nextDouble();
+ tasi = (suuzi1+suuzi2);
+ hiki = (suuzi1-suuzi2);
+ kake = (suuzi1*suuzi2);
+ wari = (suuzi1/suuzi2);
+ amari = (suuzi1%suuzi2);
+ System.out.println(tasi);
+ System.out.println(hiki);
+ System.out.println(kake);
+ System.out.println(wari);
+ System.out.println(amari);
+ }
+ }
